cf68ebd4c685b3854ddd69daa88a510d6daa1bb1
Lautaroflores <lautaroflores.1516@gmail.com>
2022-02-07 22:19:04 -0300

    PseudoEntrega

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,10 +1,15 @@
 import Container from '@mui/material/Container';
+import Example from './ItemCount.Js';
 
 const ItemListContainer=({greeting})=> {
   return (
+    <div>
       <Container>
         {greeting}
+      <Example/>
       </Container>
+    </div>
+    
   );
 }
 
